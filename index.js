81812afd936fdc6238246ce895621cdd9c7ec19d
Athul MS <athulms@gmail.com>
2021-08-15 19:58:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3004;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get('/test', async (req, res) => {
+  res.send("Ok");
+});
+
+app.listen(port, () =>
+  console.log(`API Server Running On Port -> ${port}!`)
+);
